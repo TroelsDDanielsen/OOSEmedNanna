f50291d6f3be391d7fdb3924d208c800934de2eb
Troels Danielsen <troelsdd@gmail.com>
2016-06-07 21:06:10 +0200

    //Jeg hedder hans

--- OOSEmedNanna/src/earlierExamSet/SpecialExam2016.java
+++ OOSEmedNanna/src/earlierExamSet/SpecialExam2016.java
@@ -7,7 +7,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
-
+//Jeg hedder hans!
 public class SpecialExam2016 {
 
 	//Question 4
